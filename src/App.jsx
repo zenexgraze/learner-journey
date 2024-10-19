179a71c7688439c0cf42b4b6310fbb70863efe56
Bhargav Mahanta <96965737+zenexgraze@users.noreply.github.com>
2024-10-19 18:24:51 +0000

    Changed the socials

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Bhargav Mahanta",
+  title: "Undergraduate Student",
+  email: "bhargavmahanta30@gmail.com",
+  gitHub: "zenexgraze",
+  instagram: "__le_zz__",
+  linkedIn: "bhargavmahanta",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "zenexgarze",
+  youTube: "bhargavmahanta",
 };
 
 const primaryColor = "#4E567E";
